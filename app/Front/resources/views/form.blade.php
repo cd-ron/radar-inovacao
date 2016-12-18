e88cc4aeabd582015e58aa1421847a52f21e90e4
Felipe Fontana <felipefontanaf@gmail.com>
2016-12-17 22:19:38 -0300

    form

--- app/Front/resources/views/form.blade.php
+++ app/Front/resources/views/form.blade.php
@@ -6,59 +6,42 @@
         </h2>
     </div>
 </div>
-<div class="col-lg-10 col-lg-offset-1">
+<div class="col-md-10 col-md-offset-1">
     <div class="form-area">
         <form role="form">
+            <div class="row">
+                <div class="form-group col-md-10">
+                    <label >Nome do evento</label>
+                    <input type="text" name="titulo" class="form-control" placeholder="Título do evento" aria-describedby="basic-addon1">
+                </div>
 
-            <label >Sobre o evento</label>
-
-            <div class="form-group">
-                <input type="text" name="titulo" class="form-control" placeholder="Título do evento" aria-describedby="basic-addon1">
-            </div>
-
-            <div class="form-group">
-                <span >Evento gratuito:
-                    <input type="checkbox" name="eventogratuito">
-                </span>
-            </div>
-
-            <div class="form-group">
-                <div class="row">
-                    <div class="col-md-6">
-                        <label>Data inicial</label>
-                        <input type="date" class="form-control" placeholder="Data de início">
-                    </div>
-
-                    <div class="col-md-6">
-                        <label>Hora inicial</label>
-                        <input type="time" class="form-control" placeholder="Hora de início">
-                    </div>
+                <div class="form-group col-md-2" style="margin-top:27px">
+                    <label >Evento gratuito: <input type="checkbox" name="eventogratuito">
+                    </label>
+                    <!--input type="checkbox" name="eventogratuito" class="form-control"-->
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-4 form-group">
+                    <label>Data de início:</label>
+                    <input type="date" class="form-control" placeholder="Data de início">
+                </div>
 
-            <div class="form-group">
-                <label>Data final</label>
-                <input type="date" class="form-control" placeholder="Data de fim">
+                <div class="col-md-4 form-group">
+                    <label>Hora de início:</label>
+                    <input type="time" class="form-control" placeholder="Hora de início">
+                </div>
+                <div class="col-md-4 form-group">
+                    <label>Data de término:</label>
+                    <input type="date" class="form-control" placeholder="Data de fim">
+                </div>
             </div>
-    
-            <label for="basic-url">Your vanity URL</label>
-            <div class="form-group">
-                <label>Data final</label>
-                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+            <div class="row">
+                <div class="form-group col-md-12">
+                    <label>Local:</label>
+                    <input type="text" class="form-control" placeholder="Local">
+                </div>
             </div>
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon1">
-            </div>
-
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon2">
-            </div>
-
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon3">
-            </div>
-
-            <button class="btn btn-primary">Salvar</button>
         </form>
     </div>
 </div>
